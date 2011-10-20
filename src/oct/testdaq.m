fc42de94f8eadff0fbbdea3048b4f6443755b7ff
Johannes Niediek <johannes.niediek@googlemail.com>
2011-10-20 17:40:55 +0200

    some more tests

--- src/oct/testdaq.m
+++ src/oct/testdaq.m
@@ -1,5 +1,20 @@
+function t = testdaq()
 try 
-	daq1208fs('i')
+	daq1208fs('i');
 catch
 	lasterror.message
+	return
 end
+
+data(1:256) = randperm(256) - 1;
+data(257:512) = randperm(256) - 1;
+t = zeros(512,1);
+for i = 1:512
+	tic
+	daq1208fs('w', data(i));
+	daq1208fs('w', 0);
+	t(i) = toc
+	sleep(.1)
+end
+
+daq1208fs('d');
